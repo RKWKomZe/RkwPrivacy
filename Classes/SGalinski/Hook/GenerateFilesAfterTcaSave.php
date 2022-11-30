551346775c4d0347dee3756d073474956994d740
Steffen Kroggel <kroggel@rkw.de>
2022-11-30 12:12:56 +0100

    [CHANGE] Cleanup for TYPO3 v9.5

--- Classes/SGalinski/Hook/GenerateFilesAfterTcaSave.php
+++ Classes/SGalinski/Hook/GenerateFilesAfterTcaSave.php
@@ -24,7 +24,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * Class GenerateFilesAfterTcaSave
  *
  * @author Steffen Kroggel <developer@steffenkroggel.de>
- * @copyright Rkw Kompetenzzentrum
+ * @copyright RKW Kompetenzzentrum
  * @package RKW_RkwPrivacy
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
